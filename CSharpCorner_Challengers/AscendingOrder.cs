64821db1e1e4f2c5ac2bbaf128da1bc8dc4cbb6b
Pascal How <pascal.how@ttgtt.com.au>
2015-08-04 15:13:32 +0100

    Added ascending order challenge

---
+++ CSharpCorner_Challengers/AscendingOrder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpCorner_Challengers
+{
+    class AscendingOrder
+    {
+        public int[] SortAscending(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i+1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
